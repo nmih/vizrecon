69e9bb7e6839f414153fcd2e51a21c0a038a1f4f
rohangro <rgrover@ucsd.edu>
2018-02-07 10:06:59 -0800

    Removing unnecessary files

--- HTML-testbed/routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const pg = require('pg');
-const path = require('path');
-
-router.get('/', (req, res, next) => {
-    res.sendFile('index.html');
-  });
