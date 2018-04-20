f13795090e65baf31361fec99627b11b0522763e
rohangro <rgrover@ucsd.edu>
2018-04-20 14:41:10 -0700

    added node stuff

---
+++ HTML-final/server.js
@@ -0,0 +1,101 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+const { Client } = require('pg');
+// Pooling is fairly trivial but will be necessary for our web application
+// const { Pool } = require('pg');
+// const pool = new Pool()
+
+const client = new Client({
+    host: 'ec2-54-235-220-220.compute-1.amazonaws.com',
+    user: 'mlkxucedzbnozx',
+    password: '054e5f07f8b5160946e2232ec93d1e03d8dd0eee1acefdec0b66992b41aa5645',
+    database: 'd36e9jg3mk08tc',
+    ssl: true,
+});
+
+client.connect();
+// Issue: not sure how to correctly end connection right now
+// Might become easier with pools as well
+
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
+
+// make express look in the public directory for assets (css/js/img)
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// set the home page route
+app.listen(port, function (err) {
+    if (err) throw err;
+    console.log('Our app is running on http://localhost:' + port);
+});
+
+app.post('/endpoint', function (req, res) {
+    var obj = {};
+    console.log('body: ' + JSON.stringify(req.body));
+    res.send(req.body);
+});
+
+app.post('/runit', function (req, res) {
+    var jon = JSON.stringify(req.body.title)
+    jon = jon.slice(1, 6);
+    console.log('body: ' + jon);
+    var obj = GetUniprot(client, jon);
+    console.log('result is: ' + JSON.stringify(obj))
+    res.send(obj);
+});
+
+app.get('/thispoint', function (req, res, err) {
+    console.log('query: ' + JSON.stringify(req.query));
+
+    var jon = JSON.stringify(req.query.data)
+    jon = jon.slice(1, 6);
+    console.log(jon)
+    var regex1 = RegExp('(b)(\\d)(\\d)(\\d)(\\d)', 'i')
+    if (regex1.test(jon) == false) {
+        var obj = {};
+        obj.geneInfo = "None";
+        res.header('Content-type', 'application/json');
+        res.header('Charset', 'utf8');
+        res.send(req.query.callback + '(' + JSON.stringify(obj) + ');');
+        if (err) throw err;
+    } else {
+        var sql = {
+            text: "SELECT a.uniprot_id, a.name_id FROM protein a WHERE gene_id='" + jon + "'",
+        };
+
+        client.query(sql, function (err, resu, callback) {
+            var obj = {};
+            var geneInfo = resu.rows;
+            console.log("The protein name is " + geneInfo[0].name_id);
+            obj = geneInfo;
+            res.header('Content-type', 'application/json');
+            res.header('Charset', 'utf8');
+            res.send(req.query.callback + '(' + JSON.stringify(obj) + ');');
+        });
+    };
+});
+
+app.get('/search', function (req, res, err) {
+    var searchStr = JSON.stringify(req.query.data);
+    searchStr = searchStr.slice(1, -1);
+    var splitArray = searchStr.split(" ");
+    var concStr = splitArray.join('%');
+    concStr = "'%" + concStr + "%'";
+    console.log(concStr);
+    var sql = {
+        text: "SELECT gene_id, name_id, description FROM protein WHERE gene_id||name_id||description ILIKE" + concStr + "LIMIT 15"
+    };
+
+    client.query(sql, function (err, resu, callback) {
+        var obj = {};
+        var searchResult = resu.rows;
+        res.header('Content-type', 'application/json');
+        res.header('Charset', 'utf8');
+        res.send(req.query.callback + '(' + JSON.stringify(searchResult) + ');');
+    });
+});
